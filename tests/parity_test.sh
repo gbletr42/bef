a7f6c9e98fb31c486a5d125654f2d254c297f4ac
gbletr42 <gbletr42@gmail.com>
2024-03-08 19:47:32 -0500

    That should also test if the parities work

--- tests/parity_test.sh
+++ tests/parity_test.sh
@@ -5,7 +5,11 @@ for parity in jerasure-vand jerasure-cauchy liberasurecode-vand intel-vand intel
 do
 	for file in test1 test2 test3
 	do
-		if ! cmp $file <(bef -c -P $parity -i $file | bef -d)
+		bef -c -P $parity -i $file -o $file.bef
+		#by default, we should be secure against 4K burst corruption
+		dd if=/dev/zero of=$file.bef bs=4K count=1 oseek=4 conv=notrunc
+
+		if ! cmp $file <(bef -d -i $file.bef)
 		then
 			echo "parity test for $parity on $file failed!"
 		fi

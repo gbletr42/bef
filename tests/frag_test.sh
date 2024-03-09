b2a26dd03f7b700f9429202dbcce1344b0289200
gbletr42 <gbletr42@gmail.com>
2024-03-08 19:57:54 -0500

    That'd be a _little_ excessive

--- tests/frag_test.sh
+++ tests/frag_test.sh
@@ -4,9 +4,9 @@
 
 for k in 1 2 4 8 16 32 64 128
 do
-	for m in 1 2 4 8 16 32 64 128
+	for m in 1 2 4 8 16 32 64 128 #Eh do I really want 512G in my tests?
 	do
-		for file in test1 test2 test3
+		for file in test1 test2 #removed test3 because I *don't want it*
 		do
 			if ! cmp $file <(bef -c -k $k -m $m -i $file | bef -d)
 			then

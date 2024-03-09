61aabc61f5b1181f325b0804c3fa0e5ba286cd37
gbletr42 <gbletr42@gmail.com>
2024-03-08 19:43:08 -0500

    oops

--- tests/bsize_test.sh
+++ tests/bsize_test.sh
@@ -5,7 +5,7 @@ for bsize in 1K 2K 4K 8K 16K 32K 64K 128K 256K 512K 1M 2M 4M 8M 16M 32M 64M 128M
 do
 	for file in test1 test2 test3
 	do
-		if ! cmp $file <(bef -c -b $bsize -i $file | bed -d)
+		if ! cmp $file <(bef -c -b $bsize -i $file | bef -d)
 		then
 			echo "block size test $bsize on $file failed!"
 		fi

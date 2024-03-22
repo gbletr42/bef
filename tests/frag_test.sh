7e8a93f48c79e0e8ee684edc3b5d8c4fcdc1d41f
gbletr42 <gbletr42@gmail.com>
2024-03-21 22:34:38 -0400

    Add uber-large k/m ratios using wirehair to tests

--- tests/frag_test.sh
+++ tests/frag_test.sh
@@ -16,4 +16,20 @@ do
 	done
 done
 
+#Test very large k and m values with wirehair, almost to max
+#k caps at 64000 for wirehair, m at 65535
+for k in 255 256 511 512 1023 1024 2047 2048 4095 4096 8191 8192 16383 16384 32767 32768 63999 64000
+do
+	for m in 255 256 511 512 1023 1024 2047 2048 4095 4096 8191 8192 16383 16384 32767 32768 65534 65535
+	do
+		for file in test1 test2
+		do
+			if ! cmp $file <(bef -c -P wirehair -k $k -m $m -i $file | bef -d)
+			then
+				echo "fragment test for k: $k, m: $m, $file failed!"
+			fi
+		done
+	done
+done
+
 

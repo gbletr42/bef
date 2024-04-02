4237faa84ca42a89c4d33b90e941c37b393fad96
gbletr42 <gbletr42@gmail.com>
2024-04-02 19:31:48 -0400

    Add new hashes to test

--- tests/hash_test.sh
+++ tests/hash_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for hash in sha1 sha256 sha3 blake2s blake3 md5 crc32 xxhash
+for hash in sha1 sha256 sha512 sha3 blake2s blake2b blake3 md5 crc32 crc32c xxhash
 do
 	for file in test1 test2 test3
 	do

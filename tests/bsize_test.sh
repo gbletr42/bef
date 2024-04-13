b80728382d2942044f3bed0a29fd6da45ca5262a
gbletr42 <gbletr42@gmail.com>
2024-04-13 19:19:21 -0400

    Make memory intensive tests use up all memory

--- tests/bsize_test.sh
+++ tests/bsize_test.sh
@@ -5,11 +5,11 @@ for ((bsize = 1024; bsize <= $((1024 * 1024 * 1024)); bsize = bsize * 2))
 do
 	for file in test1 test2 test3
 	do
-		if ! cmp $file <(bef -c -l 1 -b $bsize -i $file | bef -d)
+		if ! cmp $file <(bef -c -L50 -l 1 -b $bsize -i $file | bef -d -L50)
 		then
 			echo "block size test $bsize on $file failed!"
 		fi
-		if ! cmp $file <(bef -c -l 1 -b $(($bsize + 1)) -i $file | bef -d)
+		if ! cmp $file <(bef -c -L50 -l 1 -b $(($bsize + 1)) -i $file | bef -d -L50)
 		then
 			echo "block size test $(($bsize + 1)) on $file failed!"
 		fi

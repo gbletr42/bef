27b8cf812425e729dc6ff89a2c923a3af67f06a3
gbletr42 <gbletr42@gmail.com>
2024-04-21 01:40:37 -0400

    oops

--- tests/upgrade_test.sh
+++ tests/upgrade_test.sh
@@ -3,7 +3,7 @@
 
 for file in test1 test2 test3
 do
-	if ! cmp <(bef -c -L50 -u 0 -i $file | bef -d -L50)
+	if ! cmp $file <(bef -c -L50 -u 0 -i $file | bef -d -L50)
 	then
 		bef -c -S -u 0 -i $file -o ${file}.bef
 		bef -d -S -i ${file}.bef -o ${file}.dec
